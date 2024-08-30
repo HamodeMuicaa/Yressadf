c4e01b464ca2c78d885bc08be725d60d6957b0be
HamodeMuicaa <arauao8881aa@gmail.com>
2024-08-30 21:00:16 +0000

    Update config.py

--- config.py
+++ config.py
@@ -42,8 +42,8 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/yousef1yt/My_Source-black-bot")
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/HamodeMuicaa/Yressadf")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN")
 CH_US = getenv("CH_US", "cecrr")
 CHANNEL_SUDO = getenv("CHANNEL_SUDO", "ceccr")
